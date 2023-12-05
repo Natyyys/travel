3a673d67b3487e99388085a4fa409bfec4354e48
Наталья Моисеева <mns-fw@bk.ru>
2023-12-05 20:17:45 +0300

    fix(tours): корректирует tours- slider.js

--- source/js/modules/tours/tours-slider.js
+++ source/js/modules/tours/tours-slider.js
@@ -1,44 +1,34 @@
-import Swiper from '../vendor/swiper';
+import Swiper from '../../vendor/swiper';
 
 const toursSlider = document.querySelector('[data-tours-slider]');
 const buttonNext = document.querySelector('[data-tours-prev]');
 const buttonPrev = document.querySelector('[data-tours-next]');
 
-function setToursSlider() {
-  return new Swiper(toursSlider, {
+const initToursSlider = () => {
+  let swiper = new Swiper(toursSlider, {
     navigation: {
       nextEl: buttonNext,
       prevEl: buttonPrev,
     },
-
-    loop: true,
     breakpoints: {
+      320: {
+        slidesPerView: 1,
+        allowTouchMove: true,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 18,
+        allowTouchMove: true,
+      },
       1200: {
         slidesPerView: 3,
         spaceBetween: 30,
         allowTouchMove: false,
-        initialSlide: 4,
-      },
-
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 18,
-        initialSlide: 5,
-      },
-
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 0,
-        initialSlide: 6,
       },
     },
   });
-}
 
-const initToursSlider = () => {
-  if (toursSlider) {
-    setToursSlider();
-  }
+  return swiper;
 };
 
 export {initToursSlider};
