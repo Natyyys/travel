f032fbd24ea74474b875942064da22ffdd456a05
Наталья Моисеева <mns-fw@bk.ru>
2023-12-26 23:13:07 +0300

    тест style-js toggle

--- source/js/modules/header-menu/toggle.js
+++ source/js/modules/header-menu/toggle.js
@@ -8,19 +8,17 @@ const addToggleMenu = () => {
   const toggle = root.querySelector('.toggle');
   const nav = root.querySelector('.nav');
 
-  const onDocumentKeydown = (evt) => {
-    return evt.key === 'Escape' ? closeMenu() : null;
+  const onDocumentKeydown = (evt) => evt.key === 'Escape' ? closeMenu() : null;
+
+  const onLinkClick = (evt) => evt.target.matches(LINK_CLASS) ? closeMenu() : null;
+
+  const isMenu = (evt) => (evt.target.closest('.header') && evt.target.closest('.toggle') || evt.target.closest('.nav')) ? evt.preventDefault() : closeMenu();
+
+  const onDocumentOutside = () => {
+    closeMenu();
   };
 
-  const onLinkClick = (evt) => {
-    return evt.target.matches(LINK_CLASS) ? closeMenu() : null;
-  };
-
-  const isMenu = (evt) => {
-    return (evt.target.closest('.header') && evt.target.closest('.toggle') || evt.target.closest('.nav')) ? evt.preventDefault() : closeMenu();
-  };
-
-  const openMenu = () => {
+  function openMenu() {
     container.classList.add(OPENED_CLASS);
     toggle.classList.add(OPENED_CLASS);
     nav.classList.add(OPENED_CLASS);
@@ -29,10 +27,9 @@ const addToggleMenu = () => {
     document.addEventListener('click', isMenu);
     ScrollLock.disableScrolling();
     document.addEventListener('click', onDocumentOutside);
+  }
 
-  };
-
-  const closeMenu = () => {
+  function closeMenu() {
     container.classList.remove(OPENED_CLASS);
     toggle.classList.remove(OPENED_CLASS);
     nav.classList.remove(OPENED_CLASS);
@@ -41,19 +38,9 @@ const addToggleMenu = () => {
     document.removeEventListener('click', isMenu);
     ScrollLock.enableScrolling();
     document.removeEventListener('click', onDocumentOutside);
-  };
+  }
 
-  const onDocumentOutside = (evt) => {
-    if (evt.target === root || root.contains(evt.target)) {
-      return;
-    } else {
-      closeMenu();
-    }
-  };
-
-  toggle.addEventListener('click', () => {
-    return !toggle.classList.contains(OPENED_CLASS) ? openMenu() : closeMenu();
-  });
+  toggle.addEventListener('click', () => !toggle.classList.contains(OPENED_CLASS) ? openMenu() : closeMenu());
 };
 
 export {addToggleMenu};
